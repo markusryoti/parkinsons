cad40460ee897f7499b14ca160ae6deb29022dc3
Markus Ryöti <markus.ryoti@student.tut.fi>
2022-09-10 19:21:44 +0300

    add separate test and validation sets

--- parkinsons.py
+++ parkinsons.py
@@ -1,4 +1,3 @@
-import os
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -32,11 +31,12 @@ if str(device) == "cuda:0":
 
 
 class ParkinsonImageDataset(Dataset):
+    CLASSES = {0: "healthy", 1: "parkinson"}
+
     def __init__(self, data, transform=None):
         self.data = data
         self.transform = transform
         self.labels = {"healthy": 0, "parkinson": 1}
-        self.classes = {0: "healthy", 1: "parkinson"}
 
     def __len__(self):
         return len(self.data)
@@ -58,6 +58,18 @@ class ParkinsonImageDataset(Dataset):
 
 def create_data(X, y):
     return pd.DataFrame({'filename': X, 'healthy': y})
+
+
+def split_train_data(df):
+    df = df.sample(frac=1)
+    num_val = int(len(df) * 0.2)
+
+    val = df.iloc[:num_val]
+    train = df.iloc[num_val:]
+
+    assert (len(val) + len(train) == len(df))
+
+    return train, val
 
 
 def train_model(model, dataloaders, criterion, optimizer, scheduler, dataset_sizes, num_epochs=25):
@@ -126,10 +138,11 @@ def train_model(model, dataloaders, criterion, optimizer, scheduler, dataset_siz
 
     # load best model weights
     model.load_state_dict(best_model_wts)
+
     return model
 
 
-def visualize_model(model, class_names, num_images=6):
+def visualize_model(model, dataloaders, class_names, num_images=6):
     was_training = model.training
     model.eval()
     images_so_far = 0
@@ -158,6 +171,27 @@ def visualize_model(model, class_names, num_images=6):
         model.train(mode=was_training)
 
 
+def test_model(model, test_data, test_set_sizes):
+    model.eval()
+
+    running_corrects = 0
+
+    with torch.no_grad():
+        for inputs, labels in test_data:
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            outputs = model(inputs.float())
+            _, preds = torch.max(outputs, 1)
+
+            running_corrects += torch.sum(preds == labels.data)
+
+    acc = running_corrects.double() / test_set_sizes
+
+    print()
+    print(f'Test set acc: {acc:.4f}')
+
+
 if __name__ == "__main__":
     transforms = {
         "train": transforms.Compose([
@@ -165,25 +199,32 @@ if __name__ == "__main__":
         ]),
         "val": transforms.Compose([
             transforms.Resize((192, 192))
+        ]),
+        "test": transforms.Compose([
+            transforms.Resize((192, 192))
         ])
     }
 
     X_train, X_test, y_train, y_test = get_files('data/drawings')
     train_data = create_data(X_train, y_train)
+    train_data, val_data = split_train_data(train_data)
     test_data = create_data(X_test, y_test)
 
     image_datasets = {
         'train': ParkinsonImageDataset(train_data, transforms['train']),
-        'val': ParkinsonImageDataset(train_data, transforms['train'])
+        'val': ParkinsonImageDataset(val_data, transforms['val']),
+        'test': ParkinsonImageDataset(test_data, transforms['test'])
     }
 
     dataloaders = {
         'train': DataLoader(image_datasets['train'], batch_size=6, shuffle=True),
-        'val': DataLoader(image_datasets['val'], batch_size=6, shuffle=True)
+        'val': DataLoader(image_datasets['val'], batch_size=6, shuffle=True),
+        'test': DataLoader(image_datasets['test'], batch_size=6, shuffle=True)
     }
 
-    dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
-    class_names = image_datasets['train'].classes
+    dataset_sizes = {x: len(image_datasets[x])
+                     for x in ['train', 'val', 'test']}
+    class_names = ParkinsonImageDataset.CLASSES
 
     model_ft = models.resnet18(weights=ResNet18_Weights.DEFAULT)
     num_ftrs = model_ft.fc.in_features
@@ -203,4 +244,6 @@ if __name__ == "__main__":
     model_ft = train_model(model_ft, dataloaders, criterion, optimizer_ft,
                            exp_lr_scheduler, dataset_sizes, num_epochs=5)
 
-    visualize_model(model_ft, class_names)
+    # visualize_model(model_ft, dataloaders, class_names)
+
+    test_model(model_ft, dataloaders['test'], dataset_sizes['test'])
